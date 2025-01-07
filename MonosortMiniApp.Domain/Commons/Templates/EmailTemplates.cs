f0b69a8d859d17132c3a677681e85c9f450af42b
Denis <ponomarevd61@gmail.com>
2025-01-07 16:06:40 +0300

    add ref for confirm email

--- MonosortMiniApp.Domain/Commons/Templates/EmailTemplates.cs
+++ MonosortMiniApp.Domain/Commons/Templates/EmailTemplates.cs
@@ -15,7 +15,7 @@ public static class EmailTemplates
         <h3 >Благодарим вас за регистрацию в нашем приложении.</h3>
         <br>
         <p >Для завершения процесса регистрации, пожалуйста, перейдите по <span>
-            <a href=""http://185.48.250.104:5257/api/User/confirm?email=@email"">ССЫЛКЕ</a>
+            <a href=""http://85.208.87.10:80/api/user/confirm?email=@email"">ССЫЛКЕ</a>
         </span> для подтверждения email.</p>
         
     </div>
