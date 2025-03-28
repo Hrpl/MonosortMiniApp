05bf6224ec26372e4f399968e04ff743ca4ac14d
Hrpl <ponomarevd61@gmail.com>
2025-03-28 18:44:46 +0300

    edit scheme

--- MonosortMiniApp.Infrastructure/Services/Implimentations/UserService.cs
+++ MonosortMiniApp.Infrastructure/Services/Implimentations/UserService.cs
@@ -10,7 +10,7 @@ namespace MonosortMiniApp.Infrastructure.Services.Implimentations;
 public class UserService : IUserService
 {
     private readonly QueryFactory _query;
-    private readonly string TableName = "public.Users";
+    private readonly string TableName = "dictionary.Users";
     public UserService(IDbConnectionManager connectionManager)
     {
         _query = connectionManager.PostgresQueryFactory;
